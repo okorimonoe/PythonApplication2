4d82de5124baf1d76ec7fc72940f762b43a7049a
okorimonoe <lolotclt@gmail.com>
2024-01-31 12:53:27 +0600

    while loops

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,50 +1,27 @@
-a = 50
-b = 10
-if a > b:
-  print("Hello World")
+i = 1
+while  i < 6:
+ print(i)
+ i += 1
  
-
-a = 50
-b = 10
-if a !=b:
-  print("Hello World")
+i = 1
+while i < 6:
+  if i == 3:
+    break
+  i += 1
   
-
-a = 50
-b = 10
-if a ==b:
-  print("Yes")
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  
+i = 1
+while i < 6:
+  print(i)
+  i += 1
 else:
-  print("No")
-  
-a = 50
-b = 10
-if a == b:
-  print("1")
-elif a > b:
-  print("2")
-else:
-  print("3")
-  
-
-if a == b and c == d:
-  print("Hello")
-  
-
-if a == b or c == d:
-  print("Hello")
-  
-a = 2
-b = 5
-print("YES") if a == b else  print("NO")
-
-a = 2
-b = 50
-c = 2
-if a == c or b == c:
-
-  print("YES")
-  
+  print("i is no longer less than 6")
 
 
 
