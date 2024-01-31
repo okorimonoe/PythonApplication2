853dfb91e891bc0ed212fec2acee6e17b8a3c82b
okorimonoe <lolotclt@gmail.com>
2024-01-31 12:21:49 +0600

    Operators

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,18 +1,11 @@
-x = "Hello"
-y = 15
-
-print(bool(x))
-print(bool(y))
-print(bool("abc"))
-print(bool(123))
-print(bool(["apple", "cherry", "banana"]))
-
-
-print ("False values:")
-print(bool(False))
-print(bool(None))
-print(bool(0))
-print(bool(""))
-print(bool(()))
-print(bool([]))
-print(bool({}))
+print(10*5)
+print(10/2)
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+  
+if 5 != 10:
+  print("5 and 10 is not equal")
+  
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
