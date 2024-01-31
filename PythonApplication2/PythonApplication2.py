c15795015d7a4a0a548453cfee0568f72d81cd39
okorimonoe <lolotclt@gmail.com>
2024-01-31 12:35:27 +0600

    list

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,11 +1,47 @@
-print(10*5)
-print(10/2)
-fruits = ["apple", "banana"]
-if "apple" in fruits:
-  print("Yes, apple is a fruit!")
-  
-if 5 != 10:
-  print("5 and 10 is not equal")
-  
-if 5 == 10 or 4 == 4:
-  print("At least one of the statements is true")
+mylist = list(("213", '321', '3211', '321', 'acv', "true"))
+print (mylist)
+print (len(mylist))
+print (type(mylist))
+print(mylist[3])
+print(mylist[-1])
+print(mylist[2:5])
+print(mylist[:5])
+print(mylist[3:])
+mylist[1] = "5421"
+print(mylist)
+
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi"
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+
+
+
+
+
+
+
+
