a51b8fb00f458aea7d492b8abba8e5f189754bce
okorimonoe <lolotclt@gmail.com>
2024-01-31 12:38:02 +0600

    tuples

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,41 +1,17 @@
-mylist = list(("213", '321', '3211', '321', 'acv', "true"))
-print (mylist)
-print (len(mylist))
-print (type(mylist))
-print(mylist[3])
-print(mylist[-1])
-print(mylist[2:5])
-print(mylist[:5])
-print(mylist[3:])
-mylist[1] = "5421"
-print(mylist)
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
 
 
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
 
-fruits = ["apple", "banana", "cherry"]
-print(fruits[1])
-
-fruits = ["apple", "banana", "cherry"]
-fruits[0]= "kiwi"
-
-fruits = ["apple", "banana", "cherry"]
-fruits.append("orange")
-
-fruits = ["apple", "banana", "cherry"]
-fruits.insert(1,"lemon")
-
-fruits = ["apple", "banana", "cherry"]
-fruits.remove("banana")
-
-fruits = ["apple", "banana", "cherry"]
+fruits = ("apple", "banana", "cherry")
 print(fruits[-1])
 
-fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(fruits[2:5])
 
-
-fruits = ["apple", "banana", "cherry"]
-print(len(fruits))
 
 
 
